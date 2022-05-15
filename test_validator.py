c9dde24ad408dd56989eeb1f80b7d55dbe52b890
root <root@LAPTOP-QHNNN60G.localdomain>
2022-05-15 19:58:58 +0200

    making testing to validator functions

--- test_validator.py
+++ test_validator.py
@@ -31,4 +31,38 @@ def test_valid_brackets(expression, expected):
     ("( ( x + 2 ) * 3 )", True)
 ])
 def test_valid_expression(expression, expected):
-    assert Validator().validate(expression) == expected
+    assert Validator().validExpression(expression) == expected
+
+
+@pytest.mark.parametrize("value , expected", [
+    ("2.5", True),
+    ("-3232.332", True),
+    ("3.3.1", False),
+    ("-1", True),
+    ("0.00", True)
+])
+def test_valid_numeric_value(value, expected):
+    assert Validator().numericValue(value) == expected
+
+
+@pytest.mark.parametrize("x_min , x_max , expected", [
+    ("12", "13", True),
+    (12, "13", True),
+    (13, 10, False),
+    ("12.2.2", "15", False),
+    (12, 12, False)
+])
+def test_valid_range(x_min, x_max, expected):
+    assert Validator().validRange(x_min, x_max) == expected
+
+
+@pytest.mark.parametrize("expression, x_min, x_max, expected", [
+    ("x^2", 2, 3, True),
+    ("x ^ 2 + (2 * x + 5 - 3) + 1", 10, 100, True),
+    ("x + 1 + ", 1, 5, False),
+    ("x", 1, 1, False),
+    ("x - -1 + 5", 1, -1, False),
+    ("x - -1 + 5", -10, -1, True),
+])
+def test_valid_input(expression, x_min, x_max, expected):
+    assert Validator().validInput(expression, x_min, x_max) == expected
